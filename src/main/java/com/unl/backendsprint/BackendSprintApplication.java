20d627ee5d89b1d074801538a54f78c856a9641e
Alex_Nole <snole7@gmail.com>
2022-06-22 23:24:28 -0500

    firts commit

---
+++ src/main/java/com/unl/backendsprint/BackendSprintApplication.java
@@ -0,0 +1,13 @@
+package com.unl.backendsprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSprintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSprintApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unl/backendsprint/BackendSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unl.backendsprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSprintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
